fe94ffb1937fcfbf11f6f414d24182b046d3119c
ChiAkuma <ChiAkuma@gmail.com>
2018-06-06 21:37:52 +0200

    Initial

---
+++ ChatApplication/src/de/polygondev/init/init.java
@@ -0,0 +1,16 @@
+package de.polygondev.init;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class init extends Application {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public void start(Stage stage) {
+        
+    }
+    
+}
